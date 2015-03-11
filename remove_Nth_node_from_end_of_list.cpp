49fdf4f460ba1f944baaa694b148b60e4438107b
SnailTowarThesun <hanvskun@hotmail.com>
2015-03-11 23:58:31 +0800

    NO19

---
+++ remove_Nth_node_from_end_of_list.cpp
@@ -0,0 +1,26 @@
+/*
+   Given a linked list,remove the Nth node from the end of list and return list head
+   For example
+		Given linked list: 1->2->3->4->5 ,and n = 2
+		After removing the second node from the end, the linked list becomes 1->2->3->5
+ */
+class Solution{
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n)
+	{
+		if(head == NULL) return head;
+		ListNode* pr = pe = head;
+		for(int i = 0; i <=n; i++)
+		{
+			if(!pe) return head->next;
+			pe = pe->next;
+		}
+		while(pe != NULL)
+		{
+			pe = pe->next;
+			pr = pr->next;
+		}
+		pr->next = pr->next->next;
+		return head;
+	}
+};
