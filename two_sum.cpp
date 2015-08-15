2f666735bf9a6e2404df66591d9afaf132e6f051
SnailTowarThesun <hanvskun@hotmail.com>
2015-08-15 10:38:10 +0800

    No.8

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,38 +1,41 @@
-/*
- Given an array of integers, find two numbers such that they add up to a specific target number.
- The function twoSum should return indices of the tow numbers such that they add up to the target,where index1 must be less than index2,Please note that your returned answers(both index1 and index2) are not zero-based.
- You may assume that each input would have exactly one solution.
-Input:numbers = {2,7,11,15},target = 9
-Output: index1 = 1, index2 = 2
- */
-
-#include <iostream>
+#include <stdio.h>
+#include <map>
 #include <vector>
 using namespace std;
 
-class solution
+/*
+ * Given an array of integers,find two numbers such that they add up to a specific target number
+ * The function twoSum should return indices of the two numbers such that they add up to the target,where index1 must be less than index2.Please note that your returned answers (both index1 and index2 are not zero-based.
+ * Your may assume that each input would have exactly one solution.
+ * Input :numbers = {2,7,11,15},target = 9
+ * Output: index1 = 2,index2 = 7
+ * */
+
+class Solution
 {
 public:
-	vector<int> twosum(vector<int> &number,int target)
+	vector<int> twoSum(vector<int>& nums, int target)
 	{
-		vector<int> result;
-		int index1,index2;
-		for(int i = 0; i < number.size(); i++)
+		int sum;
+		vector<int> results;
+		map<int,int> hmap;
+		for(int i = 0; i< nums.size(); ++i)
 		{
-			index1 = number[i];
-			for(int j = i+1; j < number.size(); j++)
+			hmap.insert(pair<int,int>(nums[i],i));
+			if(hmap.count(target-nums[i]))
 			{
-				if(index1 + number[j] == target)
+				int n = hmap[target-nums[i];
+				if(n < i)
 				{
-					result.push_back(index1 > number[j] ? number[j] : index1);
-					result.push_back(index1 == result[0] ? number[j] : index1);
+					results.push_back(n+1);
+					results.push_back(i+1);
+					return results;
 				}
-
 			}
 		}
-		return result;
+		return results;
 	}
-};
+}
 
 int main()
 {
