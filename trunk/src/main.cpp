0b4d27081fd91ae7ad0e0aceb84399c42b1047b0
ME_Kun_Han <hanvskun@hotmail.com>
2018-03-25 09:54:15 +0800

    add merge k sorter lists

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 
@@ -8,12 +9,6 @@ using namespace std;
 int main()
 {
 	cout << "the answer of leetcode" << endl;
-
-	vector<int> nums = {1,1,-1,-1,3};
-
-	Solution s;
-
-	cout << s.threeSumClosest(nums, -1);
 
 
 	getchar();
