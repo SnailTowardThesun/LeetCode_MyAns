e6a1b1b55a7d9aa9268ee12f3b4dabc118242021
ME_Kun_Han <hanvskun@hotmail.com>
2018-03-25 09:29:27 +0800

    add 3sum closet

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -3,9 +3,18 @@
 #include <algorithm>
 using namespace std;
 
+
+
 int main()
 {
 	cout << "the answer of leetcode" << endl;
+
+	vector<int> nums = {1,1,-1,-1,3};
+
+	Solution s;
+
+	cout << s.threeSumClosest(nums, -1);
+
 
 	getchar();
 	return 0;
